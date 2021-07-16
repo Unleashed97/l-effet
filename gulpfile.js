26799ca482b1e6453f8f5b6948a39a6f6de93947
Nichkov Roman <nichkovroman@gmail.com>
2021-07-16 16:52:24 +0300

    initial, build with pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,81 +1,43 @@
 'use strict';
 
-const { src, dest } = require('gulp');
-const gulp = require('gulp');
-const htmlmin = require('gulp-htmlmin');
+const { src, dest, parallel, series, watch } = require('gulp');
+const sync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cleancss = require('gulp-clean-css');
-const rename = require('gulp-rename');
-const panini = require('panini');
 const del = require('del');
-const concat = require('gulp-concat')
-const uglify = require('gulp-uglify-es').default;
-const autoPrefixer = require('gulp-autoprefixer');
+const htmlmin = require('gulp-htmlmin');
 const imagemin = require('gulp-imagemin');
 const newer = require('gulp-newer');
-const browserSync = require('browser-sync').create();
+const rename = require('gulp-rename');
+const pug = require('gulp-pug');
 
-// PATHS
-const srcPath = 'src/';
-const distPath = 'dist/';
-
-const path = {
-    build: {
-        html:   distPath,
-        js:     distPath + "js/",
-        css:    distPath + "css/",
-        images: distPath + "images/",
-        fonts:  distPath + "fonts/"
-    },
-    src: {
-        html:   srcPath + "*.html",
-        js:     srcPath + "js/*.js",
-        css:    srcPath + "scss/*.scss",
-        images: srcPath + "images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}",
-        fonts:  srcPath + "fonts/**/*.{eot,woff,woff2,ttf,svg}"
-    },
-    watch: {
-        html:   srcPath + "**/*.html",
-        js:     srcPath + "js/**/*.js",
-        css:    srcPath + "scss/**/*.scss",
-        images: srcPath + "images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}",
-        fonts:  srcPath + "fonts/**/*.{eot,woff,woff2,ttf,svg}"
-    },
-    clean: "./" + distPath + "**/*"
+// PUG
+const pughtml = ()=>{
+    return src('src/pug/pages/**/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(dest('dist/'))
+        .pipe(sync.stream())
 }
 
-
-// browser
-const browser = () => {
-    browserSync.init({
-        server: {
-            baseDir: "./" + distPath
-        },
-        notify: false
-    });
-};
-
 // HTML
-const html = () => {
-    panini.refresh();
-    return src(path.src.html)
-        .pipe(panini({
-            root:       srcPath,
-            layouts:    srcPath + 'layouts/',
-            partials:   srcPath + 'partials/',
-        }))
-        .pipe(htmlmin({
-            removeComments: true,
-            collapseWhitespace: true
-        }))
-        .pipe(dest(path.build.html))
-        .pipe(browserSync.reload({stream: true}));
-};
+// const html = () => {
+//     return src('src/**/*.html')
+//         .pipe(htmlmin({
+//             removeComments: true,
+//             collapseWhitespace: true
+//         }))
+//         .pipe(dest('dist'))
+//         .pipe(sync.stream())
+// }
 
-// CSS
+// Styles
 const styles = () => {
-    return src('src/scss/main.scss')
+    return src(['src/scss/main.scss'])
         .pipe(sass())
         .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], cascade: false }))
         .pipe(cleancss(({ level: { 1: { specialComments: 0}}})))
@@ -83,24 +45,34 @@ const styles = () => {
             basename: 'style',
             suffix: '.min'
         }))
-        // .pipe(dest('src/css/'))
-        .pipe(dest(path.build.css))
-        .pipe(browserSync.reload({stream: true}));
-};
-
-// JS
-const scripts = () => {
-    return src(path.src.js, {base: srcPath + 'js/'})
-        .pipe(concat('script.min.js'))
-        .pipe(uglify())
-        .pipe(dest(path.build.js))
-        .pipe(browserSync.reload({stream: true}));
+        .pipe(dest('src/css/'))
+        .pipe(dest('dist/css/'))
+        .pipe(sync.stream())
 }
 
-// Images minify
-const images = () => {
-    return src(path.src.images)
-        .pipe(newer(path.build.images))
+// Scripts
+const scripts = () => {
+    return src(['src/js/script.js', 'src/js/**/*.js', '!src/js/script.min.js'])
+        .pipe(concat('script.min.js'))
+        .pipe(uglify())
+        .pipe(dest('src/js/'))
+        .pipe(dest('dist/js/'))
+        .pipe(sync.stream())
+}
+
+// server
+const server = () => {
+    sync.init({
+        server: { baseDir: 'dist/'},
+        notify: false,
+        online: true
+    })
+}
+
+// image minify
+const images = () =>{
+    return src('src/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}')
+        .pipe(newer('dist/images/'))
         .pipe(imagemin([
             imagemin.gifsicle({interlaced: true}),
             imagemin.mozjpeg({quality: 75, progressive: true}),
@@ -112,41 +84,42 @@ const images = () => {
                 ]
             })
         ]))
-        .pipe(dest(path.build.images))
-        .pipe(browserSync.reload({stream: true}));
+        .pipe(dest('dist/images/'))
+        .pipe(sync.stream())
 }
 
-// Fonts
-const fonts = () => {
-    return src(path.src.fonts)
-        .pipe(dest(path.build.fonts))
-        .pipe(browserSync.reload({stream: true}));
-} 
+// fonts
+const fonts = () =>{
+    return src('src/fonts/**/*.{eot,woff,woff2,ttf,svg}')
+        .pipe(dest('dist/fonts/'))
+        .pipe(sync.stream())
+}
 
-// clean Dist folder
-function clean() {
-    return del(path.clean, { force: true});
+// cleandist
+const cleandist = () => {
+    return del('dist/**/*', { force: true })
 }
 
 // Watch
-function watchFiles() {
-    gulp.watch([path.watch.html], html);
-    gulp.watch([path.watch.css], styles);
-    gulp.watch([path.watch.js, '!src/js/script.min.js'], scripts);
-    gulp.watch([path.watch.images], images);
-    gulp.watch([path.watch.fonts], fonts);
+const watchFiles = () => {
+    watch('src/pug/**/*.pug', pughtml);
+    watch('src/**/*.scss', styles);
+    watch(['src/**/*.js', '!src/js/script.min.js'], scripts);
+    watch('src/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}', images)
+    watch('src/fonts/**/*.{eot,woff,woff2,ttf,svg}', fonts);
+    watch('dist/**/*.html').on('change', sync.reload);
 }
 
-const build = gulp.series(clean, gulp.parallel(html, styles, scripts, images, fonts));
-const watch = gulp.parallel(build, watchFiles, browser);
+const build = series(cleandist, parallel(pughtml, styles, scripts, images, fonts));
+const watcher = parallel(build, watchFiles, server);
 
-/* Exports Tasks */
-exports.browser = browser;
-exports.html = html;
-exports.styles = styles;
+// Export tasks
+exports.server = server;
+exports.pughtml = pughtml;
 exports.scripts = scripts;
+exports.styles = styles;
 exports.images = images;
 exports.fonts = fonts;
-exports.clean = clean;
+exports.cleandist = cleandist;
 exports.build = build;
-exports.default = watch;
+exports.default = watcher;
